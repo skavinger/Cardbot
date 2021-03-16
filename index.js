4e3a7d27b7f4d995b413c6f42e7c9725636d976d
skavinger <skavinger@gmail.com>
2021-03-15 19:09:33 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -7,15 +7,39 @@ bot.on("ready", function(){
 });
 
 bot.on("message", function(message){
-    if(message.content.match(/^\?cardbot .*/)){
-        var card = message.content.match(/^\?cardbot (.*)/)[1];
-        if(cardlist.standard[card] !== undefined){
-            message.reply(cardlist.standard[card]);
-        }
-        else{
-            message.reply("Sorry, can't find " + card + ".");
+    try{
+        var cardlink;
+        if(message.content.match(/^\?cardbot .*/)){
+            var card = message.content.match(/^\?cardbot (.*)/)[1].toLowerCase();
+            if(cardlist[card] !== undefined){
+                cardlink = cardlist[card];
+            }
+            else{
+                var keys = Object.keys(cardlist);
+                for(var i = 0; i < keys.length; i++){
+                    if(keys[i].includes(card)){
+                        cardlink = cardlist[keys[i]];
+                        card = keys[i];
+                        break;
+                    }
+                }
+            }
+            if(cardlink !== undefined && cardlink !== " "){
+                message.reply(cardlink);
+            }
+            else if(cardlink === " "){
+                message.reply("Sorry " + card + " has not been uploaded yet.");
+            }
+            else{
+                message.reply("Sorry can't find " + card + ".");
+            }
         }
     }
+    catch(e){
+        console.log(e);
+        message.reply("Sorry something went wrong please contact owner");
+    }
+    
 });
 
-bot.login("ODIxMDYzMTcxMDg2NzQ1Njcy.YE-Qqg.B8npAORkrTqPsVJr1L6X9ZoRht4");
+bot.login(process.argv[2]);
